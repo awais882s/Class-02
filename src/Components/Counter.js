f15608b8ad6bca1f634af810f47f101eeaf38143
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-03-04 22:11:14 +0500

    plus and minus btn create

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,9 +1,13 @@
 function Counter() {
-  const counter = 0;
-  return (
-    <div>
-      <p> counter is :{counter}</p>
-    </div>
-  );
+    const counter = 0;
+    return (
+        <div style={{ padding: 50 }}>
+            <button>-</button>
+            <span style={{ padding: 20 }}> counter is : {counter} </span>
+            <button>+</button>
+        </div>
+    );
 }
 export default Counter;
+
+// state
